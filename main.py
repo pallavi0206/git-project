672703aa0064057ea36a34abcc03df16ce7713ba
pallu0206 <pallu.kri02@gmail.com>
2017-08-15 22:10:28 +0530

    my first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print "let's get started!"
+spy_name = raw_input("provide your name here :")
+spy_salutation = raw_input('what should we call you? :')
+# concatination of saluttion and name
+spy_name = spy_salutation + " " + spy_name
+print 'welcome'  +  spy_name  +  'luv to see you back'
+
+
